ccc6011dc9e1b7e431ff8e0c938b55d82f2e0500
Maden23 <maden98@gmail.com>
2018-04-21 02:54:52 +0300

    Movement implementation using Rigidbody

---
+++ Biathlon3D_Unity/Assets/PlayerCameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour 
+{
+	public GameObject player;
+	private Vector3 playerPos;
+
+	void Start () 
+	{
+		playerPos = player.transform.position;
+	}
+	
+	void FixedUpdate () 
+	{
+		
+	}
+}
---
+++ Biathlon3D_Unity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+	public float speed = 8;
+
+	private Vector3 direction = Vector3.zero;
+	private Animator animator;
+	private Rigidbody rb;
+
+	void Start () 
+	{
+		animator = GetComponent<Animator>();
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	private void FixedUpdate()
+	{
+		MoveCharacter ();
+	}
+	
+	private void MoveCharacter ()
+	{
+		direction.x = Input.GetAxis ("Horizontal") * speed;
+		direction.z = Input.GetAxis ("Vertical") * speed;
+
+		if (direction.x != 0 || direction.z != 0) 
+		{
+			animator.SetBool ("isMoving", true);
+			rb.AddForce(direction * Time.deltaTime);
+		} 
+		else 
+		{
+			animator.SetBool ("isMoving", false);
+		}
+	}
+}
